050c89ab7ac8c0f05293ae8fc39ae4d88f4690c3
spihill <spihill.in@gmail.com>
2019-09-17 17:25:09 +0900

    fixed

--- .github/myscript/push.sh
+++ .github/myscript/push.sh
@@ -4,6 +4,6 @@ files='files.txt'
 touch $files
 rm $files
 date > $files
-python3 listup_source.py3
+python3 listup_source.py
 git add $files
 git commit -m "update files.txt" && git push origin master
