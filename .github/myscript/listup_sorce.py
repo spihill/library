7fa76197d97a16d096e5b4da8e714ec4f0dda748
spihill <spihill.in@gmail.com>
2019-09-17 17:23:29 +0900

    change script

--- .github/myscript/listup_sorce.py
+++ .github/myscript/listup_sorce.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import glob
 
 file_name = "./files.txt"
@@ -6,14 +8,14 @@ ng_words = ['verify', 'test.cpp','test/','develop']
 
 try:
 	target = open(file_name, 'a')
-	files = glob.glob("**/*.cpp", recursive=True)
+	files = glob.glob("../../**/*.cpp", recursive=True)
 	for file in files:
 		ok = 1
 		for words in ng_words:
 			if file.find(words) != -1:
 				ok = 0
 		if ok == 1:
-			target.write(file + '\n')
+			target.write(file[6:] + '\n')
 except Exception as e:
 	print(e)
 finally:
