42a37287ebcc64f8445f0d981d4349123bb726c4
spihill <spihill.in@gmail.com>
2019-09-19 13:47:11 +0900

    test

--- datastructure/UnionFind.cpp
+++ datastructure/UnionFind.cpp
@@ -1,18 +1,9 @@
-struct UnionFind
-{
+struct UnionFind {
+	int sz;
 	vector<int> par;
-	int n;
-	UnionFind(int a) {init(a);}
-	void init(int a) {
-		n = a;
-		par.resize(n, -1);
-	}
-	int root(int x) {
-		return par[x] < 0 ? x : par[x] = root(par[x]);
-	}
-	bool same(int x, int y) {
-		return root(x) == root(y);
-	}
+	UnionFind(int a) : sz(a), par(sz, -1) {}
+	int root(int x) { return par[x] < 0 ? x : par[x] = root(par[x]);}
+	bool same(int x, int y) { return root(x) == root(y);}
 	bool unite(int x, int y) {
 		x = root(x);
 		y = root(y);
@@ -22,8 +13,6 @@ struct UnionFind
 		par[y] = x;
 		return true;
 	}
-	int size(int x) {
-		x = root(x);
-		return -par[x];
-	}
+	int size(int x) { return -par[root(x)];}
+	int size() { return sz;}
 };
