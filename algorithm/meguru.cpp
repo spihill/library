76064c3301b0a0e33c7a547a3411999ca972ab42
spihill <spihill.in@gmail.com>
2019-09-19 14:01:40 +0900

    fixed

--- algorithm/meguru.cpp
+++ algorithm/meguru.cpp
@@ -2,9 +2,8 @@
 template<class T>
 T meguru(T ok, T ng, function<bool(T)> isok) {
 	while (abs(ok - ng) > 1) {
-		T mid = (ok + ng) / 2;
-		if (isok(mid)) ok = mid;
-		else ng = mid;
+		T mid = (ok + ng) >> 1;
+		(isok(mid) ? ok : ng) = mid;
 	}
 	return ok;
 }
