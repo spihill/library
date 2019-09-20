8e1fde1bc4c0ea44c21ac317f7632f171befd399
spihill <spihill.in@gmail.com>
2019-09-20 18:29:30 +0900

    fixed

--- .github/myscript/push.sh
+++ .github/myscript/push.sh
@@ -13,7 +13,7 @@ if [[ $MY_GITHUB_ACTION_TEST ]]; then
 	rm ./* -f
 	mv ../library/library/.github/myscript/cpp.json .
 	git add cpp.json
-	stat=`git diff | wc -l`
+	stat=`git diff remotes/origin/master | wc -l`
 	if [ $stat -eq 0 ]; then
 		exit 0
 	fi
