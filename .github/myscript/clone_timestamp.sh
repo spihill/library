58920bae019c78fdc43e43cd06069cb29467ea74
spihill <spihill.in@gmail.com>
2019-10-09 12:27:30 +0900

    test

--- .github/myscript/clone_timestamp.sh
+++ .github/myscript/clone_timestamp.sh
@@ -4,7 +4,9 @@ git config --global user.name "spihill"
 git config --global user.email "spihill.in@gmail.com"
 
 if [[ $MY_GITHUB_ACTION_TEST ]]; then
-	cd ../../test/.test
+	cd ../../test/
+	mkdir .test
+	cd .test
 	git clone https://spihill:$CPP_JSON_GIST_TOKEN@gist.github.com/spihill/$TIMESTAMP_GIST_ID
 	mv $TIMESTAMP_GIST_ID timestamp
 fi
