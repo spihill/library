9be2f4d3d21b998487029cc569fcc63014e29fc4
spihill <spihill.in@gmail.com>
2019-10-09 12:29:51 +0900

    test

--- test/aoj/DynamicSetgree_RMQ.test.cpp
+++ test/aoj/DynamicSetgree_RMQ.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A"
 
 #include<bits/stdc++.h>
+
 using namespace std;
 
 #include "../../datastructure/SegmentTree/DynamicSegTree.cpp"
