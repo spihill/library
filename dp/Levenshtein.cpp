ae8760a2be8865cbba7aef68f3a7ac83148cc349
spihill <spihill.in@gmail.com>
2020-01-14 12:20:47 +0900

    fix

--- dp/Levenshtein.cpp
+++ dp/Levenshtein.cpp
@@ -1,6 +1,6 @@
 template<class T>
 int Levenshtein(const T& a, const T& b) {
-abcdvector<vector<int>> dp(a.size() + 1, vector<int>(b.size() + 1, INT_MAX));
+vector<vector<int>> dp(a.size() + 1, vector<int>(b.size() + 1, INT_MAX));
 	for (size_t i = 0; i <= a.size(); i++) dp[i][0] = i;
 	for (size_t i = 0; i <= b.size(); i++) dp[0][i] = i;
 	for (size_t i = 0; i < a.size(); i++) {
