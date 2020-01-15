0dad25ff16098fdc988c4cc1b47e54eacda6fbda
spihill <spihill.in@gmail.com>
2020-01-15 14:35:01 +0900

    add mytestaa

--- .github/myscript/clone_timestamp.sh
+++ .github/myscript/clone_timestamp.sh
@@ -4,7 +4,7 @@ git config --global user.name "spihill"
 git config --global user.email "spihill.in@gmail.com"
 
 if [[ $MY_GITHUB_ACTION_TEST ]]; then
-	cd ../../test/
+	cd ../../test/mytest
 	mkdir .test
 	cd .test
 	git clone https://spihill:$CPP_JSON_GIST_TOKEN@gist.github.com/spihill/$TIMESTAMP_GIST_ID
