ae6f7fa3fcc3b7081b5413c8c4e7d1477ffdf5ff
spihill <spihill.in@gmail.com>
2020-01-16 19:28:18 +0900

    fix

---
+++ monoid/max_monoid.cpp
@@ -0,0 +1,19 @@
+template<class T>
+struct max_monoid {
+	using mono = max_monoid;
+	max_monoid() : max_monoid(numeric_limits<T>::min()) {}
+	explicit max_monoid(T x) : val(x) {}
+	T val;
+	mono operator+(const mono& rhs) const {
+		return mono(max(val, rhs.val));
+	}
+	friend istream& operator>>(istream& lhs, mono& rhs) {
+		lhs >> rhs.val;
+		return lhs;
+	}
+	friend ostream& operator<<(ostream& lhs, mono& rhs) {
+		lhs << rhs.val;
+		return lhs;
+	}
+	using monoid_type = T;
+};
---
+++ monoid/plus_monoid.cpp
@@ -0,0 +1,19 @@
+template<class T>
+struct plus_monoid {
+	using mono = plus_monoid;
+	plus_monoid() : plus_monoid(T()) {}
+	explicit plus_monoid(T x) : val(x) {}
+	T val;
+	mono operator+(const mono& rhs) const {
+		return mono(val + rhs.val);
+	}
+	friend istream& operator>>(istream& lhs, mono& rhs) {
+		lhs >> rhs.val;
+		return lhs;
+	}
+	friend ostream& operator<<(ostream& lhs, mono& rhs) {
+		lhs << rhs.val;
+		return lhs;
+	}
+	using monoid_type = T;
+};
