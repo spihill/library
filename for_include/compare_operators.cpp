80d11b2bee038307f85f26983087514ab8a10ae1
spihill <spihill.in@gmail.com>
2020-01-24 02:04:20 +0900

    fix

--- for_include/compare_operators.cpp
+++
@@ -1,5 +0,0 @@
-	inline bool operator>(const type& rhs) const { return rhs < *this; }
-	inline bool operator>=(const type& rhs) const { return !(*this < rhs); }
-	inline bool operator<=(const type& rhs) const { return !(rhs < *this); }
-	inline bool operator==(const type& rhs) const { return !(*this < rhs) && !(rhs < *this); }
-	inline bool operator!=(const type& rhs) const { return (*this < rhs) || (rhs < *this); }
