922fe267ce07811d715bc52ce81609cf3de013f6
spihill <spihill.in@gmail.com>
2020-01-24 15:18:23 +0900

    fix

---
+++ test/aoj/FordFulkerson.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A&lang=ja"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include "../../template/FlowGraph.cpp"
+#include "../../graph/FordFulkerson.cpp"
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	auto G = make_flow_graph<long long>(n);
+	for (int i = 0; i < m; i++) {
+		int u, v, c;
+		cin >> u >> v >> c;
+		G.add_edge(u, v, c);
+	}
+	cout << FordFulkerson(G, 0, n-1) << endl;
+}
---
+++ test/yosupo/RBST_Affine_2.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#include "../../datastructure/SegmentTree/RBST.cpp"
+#include "../../monoid/affine_monoid.cpp"
+#include "../../math/ModInt.cpp"
+
+using modint = ModInt<998244353>;
+using monoid = affine_monoid<modint>;
+
+int main() {
+	int N, Q; cin >> N >> Q;
+	RBST<long long, monoid, greater<>> R;
+	for (int i = 0; i < N; i++) {
+		int a, b; cin >> a >> b;
+		R.set(-i, {a, b});
+	}
+	for (int i = 0; i < Q; i++) {
+		int q; cin >> q;
+		if (q == 0) {
+			int p, c, d; cin >> p >> c >> d;
+			R.set(-p, {c, d});
+		} else {
+			int l, r;
+			modint x;
+			cin >> l >> r >> x;
+			auto t = R.get(-l, -r);
+			cout << t.first * x + t.second << endl;
+		}
+	}
+}
