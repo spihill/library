3e0a26e2deac3179bec7d688a286585d8cd1b014
spihill <spihill.in@gmail.com>
2020-02-11 02:15:53 +0900

    fix monoid

--- monoid/update_monoid.cpp
+++ monoid/update_monoid.cpp
@@ -5,7 +5,7 @@ struct update_monoid : public monoid_base<T> {
 	using monoid = update_monoid;
 	using monoid_base<T>::monoid_base;
 	bool unit;
-	update_monoid() : val(T()), unit(true) {}
+	update_monoid() : monoid_base<T>(T()), unit(true) {}
 	monoid operator+(const monoid& rhs) const {
 		if (rhs.unit) return *this;
 		return rhs;
