05a2c03f0702f5395cda47afc13aa636a3dc7140
spihill <spihill.in@gmail.com>
2020-02-18 15:06:00 +0900

    fixed meguru

--- algorithm/meguru.cpp
+++ algorithm/meguru.cpp
@@ -1,6 +1,7 @@
 // meguru<T>(ok, ng, isok)で呼ぶ
-template<class T>
-T meguru(T ok, T ng, function<bool(T)> isok) {
+template<class T, class F>
+T meguru(T ok, T ng, F isok) {
+	static_assert(is_same<decltype(declval<F>()(declval<T>())), bool>::value, ""); 
 	while (abs(ok - ng) > 1) {
 		T mid = (ok + ng) / 2;
 		(isok(mid) ? ok : ng) = mid;
